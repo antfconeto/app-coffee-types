e10f4f81e029269e34903ecec59b25df74cab1e7
antfconeto <antfcone@gmail.com>
2025-09-05 16:11:24 -0300

    Update package.json main entry point and refine .gitignore
    - Changed the main entry point in package.json from "dist/index.d.ts" to "dist/index.js" for correct module resolution.
    - Removed specific index.js.map and index.js entries from .gitignore to streamline ignored files.

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SnsService = exports.LoggerSingleton = exports.LoggerCustom = exports.CustomError = exports.EventType = exports.MediaType = exports.EventSource = exports.SnsEventsStatus = exports.Entities = exports.SnsEventType = exports.LogTag = exports.Role = void 0;
+// Enums
+const user_1 = require("./enums/user");
+Object.defineProperty(exports, "Role", { enumerable: true, get: function () { return user_1.Role; } });
+const system_1 = require("./enums/system");
+Object.defineProperty(exports, "LogTag", { enumerable: true, get: function () { return system_1.LogTag; } });
+Object.defineProperty(exports, "SnsEventType", { enumerable: true, get: function () { return system_1.SnsEventType; } });
+Object.defineProperty(exports, "Entities", { enumerable: true, get: function () { return system_1.Entities; } });
+Object.defineProperty(exports, "SnsEventsStatus", { enumerable: true, get: function () { return system_1.SnsEventsStatus; } });
+Object.defineProperty(exports, "EventSource", { enumerable: true, get: function () { return system_1.EventSource; } });
+Object.defineProperty(exports, "MediaType", { enumerable: true, get: function () { return system_1.MediaType; } });
+Object.defineProperty(exports, "EventType", { enumerable: true, get: function () { return system_1.EventType; } });
+// Classes
+const customError_1 = require("./utils/customError/customError");
+Object.defineProperty(exports, "CustomError", { enumerable: true, get: function () { return customError_1.CustomError; } });
+const logger_1 = require("./utils/logger/logger");
+Object.defineProperty(exports, "LoggerCustom", { enumerable: true, get: function () { return logger_1.LoggerCustom; } });
+const snsService_1 = require("./utils/snsService/snsService");
+Object.defineProperty(exports, "SnsService", { enumerable: true, get: function () { return snsService_1.SnsService; } });
+const logger_2 = require("./utils/logger/logger");
+exports.LoggerSingleton = logger_2.default;
+//# sourceMappingURL=index.js.map
