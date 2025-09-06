67d677b3c1fbf7bc21227d35eafc3c1fa294e23b
antfconeto <antfcone@gmail.com>
2025-09-06 13:23:21 -0300

    Add product-related event types and update exports in system definitions
    - Introduced PRODUCT_CREATED, PRODUCT_UPDATED, and PRODUCT_DELETED to the SnsEventType enum in system.d.ts.
    - Added PRODUCT to the Entities enum for better representation of product-related data.
    - Updated index.d.ts to include the new IProduct type in exports.
    - Adjusted corresponding TypeScript declaration and map files to ensure consistency across the codebase.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,7 @@ import { Role } from "./enums/user";
 import { LogTag, SnsEventType, Entities, SnsEventsStatus, EventSource, MediaType, EventType } from "./enums/system";
 import { User, FirebaseUserData, FirebaseAuthEvent, Card, CardItem } from "./types/user";
 import { Coffee, Seller, Category, Media, Review } from "./types/coffee";
+import { IProduct } from "./types/product";
 import { ILogger, IDatabaseClient, ServiceDependecies, DaoDependecies, SnsEventPayload, SnsMessageInfo } from "./types/system";
 import { ILoggerCustom } from "./utils/logger/Ilogger";
 import { ISnsService } from "./utils/snsService/IsnsService";
@@ -9,7 +10,7 @@ import { CustomError } from "./utils/customError/customError";
 import { LoggerCustom } from "./utils/logger/logger";
 import { SnsService } from "./utils/snsService/snsService";
 import LoggerSingleton from "./utils/logger/logger";
-export type { User, FirebaseUserData, FirebaseAuthEvent, Coffee, Seller, Category, Media, Review, ILogger, IDatabaseClient, ServiceDependecies, DaoDependecies, SnsEventPayload, SnsMessageInfo, ILoggerCustom, ISnsService, Card, CardItem, };
+export type { User, FirebaseUserData, FirebaseAuthEvent, Coffee, Seller, Category, Media, Review, IProduct, ILogger, IDatabaseClient, ServiceDependecies, DaoDependecies, SnsEventPayload, SnsMessageInfo, ILoggerCustom, ISnsService, Card, CardItem, };
 export { Role, LogTag, SnsEventType, Entities, SnsEventsStatus, EventSource, MediaType, EventType, };
 export { CustomError, LoggerCustom, LoggerSingleton, SnsService, };
 //# sourceMappingURL=index.d.ts.map
