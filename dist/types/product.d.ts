047e8325c7a2ba1f54f19921a97bb6d60016ec98
antfconeto <antfcone@gmail.com>
2025-09-06 14:10:55 -0300

    Add medias property to IProduct interface in TypeScript definitions
    - Introduced `medias` property to the `IProduct` interface in `product.d.ts` to support multiple media types associated with a product.
    - Updated the corresponding TypeScript declaration map file to reflect this change and maintain consistency across the codebase.

--- dist/types/product.d.ts
+++ dist/types/product.d.ts
@@ -1,9 +1,11 @@
+import { Media } from "./coffee";
 export interface IProduct {
     id: string;
     name: string;
     description: string;
     ownerId: string;
     price: number;
+    medias: Media[];
     currency: string;
     weight: number;
     weightUnit: string;
